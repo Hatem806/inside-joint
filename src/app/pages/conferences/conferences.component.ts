73308895b3dca4859a75f271ccdf58548ab6f59e
Bassel Fouad <basselmf@gmail.com>
2020-08-10 00:07:58 +0200

    Conferences added with working webpageLink

--- src/app/pages/conferences/conferences.component.ts
+++ src/app/pages/conferences/conferences.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { RouterModule } from '@angular/router';
+import { IEventResponse } from 'src/app/models/responses/EventResponse';
+import { IEvent } from 'src/app/models/Event';
 
 @Component({
   selector: 'app-conferences',
@@ -9,13 +11,17 @@ import { RouterModule } from '@angular/router';
 })
 export class ConferencesComponent implements OnInit {
 
-
+ conferences:IEvent[]
   constructor( public apiService : ApiService , private router : RouterModule ) { }
 
   ngOnInit(): void {
-    this.apiService.getconferenceServices().getConferences().subscribe( conference => {
-      console.log(conference);
+    this.apiService.getconferenceServices().getConferences().subscribe( data => {
+      console.log(data);
+      this.conferences = data.events.slice(0,4) ;
     })
 
+  }
+  goToLink(link){
+    window.location.href = link ;
   }
 }
